83f9b9f5c806ad44e65b3be19254de403f8f142c
xiongwenting <499626079@qq.com>
2019-05-08 15:12:25 +0800

    12

---
+++ java/src/java1/PasswordAndUsernameException.java
@@ -0,0 +1,30 @@
+package java1;
+
+public class PasswordAndUsernameException extends Exception {
+
+	public PasswordAndUsernameException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordAndUsernameException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordAndUsernameException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordAndUsernameException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordAndUsernameException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
